5bb627baa486194e72df6d7d14b1ab0393d5241c
Chie888 <gizunova2005@mail.ru>
2025-02-21 05:05:44 +0500

    Add files via upload

---
+++ Program.fs
@@ -0,0 +1,25 @@
+﻿open System
+
+// Функция для нахождения максимальной цифры в числе
+let rec maxDigit (n: int) : int =
+    if n = 0 then
+        0 // Базовый случай: если число равно 0, возвращаем 0
+    else
+        let lastDigit = n % 10 // Получаем последнюю цифру числа
+        let maxOfRest = maxDigit (n / 10) // Рекурсивно вызываем функцию для оставшейся части числа
+        max lastDigit maxOfRest // Возвращаем максимум между последней цифрой и максимальной цифрой оставшейся части
+
+[<EntryPoint>]
+let main argv =
+    printfn "Введите натуральное число:"
+    let input = Console.ReadLine()
+
+    // Проверяем, является ли ввод корректным
+    match Int32.TryParse(input) with
+    | (true, number) when number > 0 -> 
+        let result = maxDigit number // Находим максимальную цифру
+        printfn "Максимальная цифра в числе %d: %d" number result
+    | _ -> 
+        printfn "Пожалуйста, введите корректное натуральное число."
+
+    0 // Код возврата
