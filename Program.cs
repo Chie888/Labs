af0173acf1b3c3cebbec47dcd6491aed807699c0
Chie888 <gizunova2005@mail.ru>
2025-04-09 04:47:26 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите длины катетов треугольника:");
+
+        double firstLeg = InputValidator.GetPositiveDouble("firstLeg = ");
+        double secondLeg = InputValidator.GetPositiveDouble("secondLeg = ");
+
+        RightTriangle triangle = new RightTriangle(firstLeg, secondLeg);
+
+        Console.WriteLine(triangle.ToString());
+        Console.WriteLine($"Площадь: {triangle.CalculateArea()}");
+
+       
+        RightTriangle newTriangle1 = ++triangle;
+        Console.WriteLine($"После ++: {newTriangle1.ToString()}");
+
+        RightTriangle newTriangle2 = --newTriangle1;
+        Console.WriteLine($"После --: {newTriangle2.ToString()}");
+
+        double area = (double)triangle;
+        Console.WriteLine($"Площадь через приведение типа: {area}");
+
+        bool isValid = (bool)triangle;
+        Console.WriteLine($"Треугольник существует: {isValid}");
+
+        RightTriangle triangle2 = new RightTriangle(3, 4);
+        Console.WriteLine($"Треугольник 1 <= Треугольник 2: {triangle <= triangle2}");
+        Console.WriteLine($"Треугольник 1 >= Треугольник 2: {triangle >= triangle2}");
+    }
+}
---
+++ RightTriangle.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+public class RightTriangle
+{
+    public double firstLeg { get; set; }
+    public double secondLeg { get; set; }
+
+
+    public RightTriangle()
+    {
+        firstLeg = 0;
+        secondLeg = 0;
+    }
+
+  
+    public RightTriangle(double first, double second)
+    {
+        this.firstLeg = first;
+        this.secondLeg = second;
+    }
+
+   
+    /// Вычисляет площадь прямоугольного треугольника по формуле 1/2 * ширина(1-й катет) * высота(2-й катет).
+    public double CalculateArea()
+    {
+        return 0.5 * firstLeg * secondLeg;
+    }
+
+    public override string ToString()
+    {
+        return $"Катеты: firstLeg = {firstLeg}, secondLeg = {secondLeg}";
+    }
+
+
+    public static RightTriangle operator ++(RightTriangle triangle)
+    {
+        return new RightTriangle(triangle.firstLeg * 2, triangle.secondLeg * 2);
+    }
+
+
+    public static RightTriangle operator --(RightTriangle triangle)
+    {
+        return new RightTriangle(triangle.firstLeg / 2, triangle.secondLeg / 2);
+    }
+
+
+    public static explicit operator double(RightTriangle triangle)
+    {
+        if (triangle.firstLeg > 0 && triangle.secondLeg > 0)
+            return triangle.CalculateArea();
+        else
+            return -1;
+    }
+
+
+    public static implicit operator bool(RightTriangle triangle)
+    {
+        return triangle.firstLeg > 0 && triangle.secondLeg > 0;
+    }
+
+  
+    public static bool operator <=(RightTriangle triangle1, RightTriangle triangle2)
+    {
+        return triangle1.CalculateArea() <= triangle2.CalculateArea();
+    }
+
+
+    public static bool operator >=(RightTriangle triangle1, RightTriangle triangle2)
+    {
+        return triangle1.CalculateArea() >= triangle2.CalculateArea();
+    }
+}
+
