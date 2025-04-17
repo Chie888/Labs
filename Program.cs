d09ddd18338aaf385e4a25af00f422fdd88401eb
Chie888 <gizunova2005@mail.ru>
2025-04-18 04:29:48 +0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,34 +1,11 @@
-﻿using System;
+using System;
+using System.Text;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Введите длины катетов треугольника:");
-
-        double firstLeg = InputValidator.GetPositiveDouble("firstLeg = ");
-        double secondLeg = InputValidator.GetPositiveDouble("secondLeg = ");
-
-        RightTriangle triangle = new RightTriangle(firstLeg, secondLeg);
-
-        Console.WriteLine(triangle.ToString());
-        Console.WriteLine($"Площадь: {triangle.CalculateArea()}");
-
-       
-        RightTriangle newTriangle1 = ++triangle;
-        Console.WriteLine($"После ++: {newTriangle1.ToString()}");
-
-        RightTriangle newTriangle2 = --newTriangle1;
-        Console.WriteLine($"После --: {newTriangle2.ToString()}");
-
-        double area = (double)triangle;
-        Console.WriteLine($"Площадь через приведение типа: {area}");
-
-        bool isValid = (bool)triangle;
-        Console.WriteLine($"Треугольник существует: {isValid}");
-
-        RightTriangle triangle2 = new RightTriangle(3, 4);
-        Console.WriteLine($"Треугольник 1 <= Треугольник 2: {triangle <= triangle2}");
-        Console.WriteLine($"Треугольник 1 >= Треугольник 2: {triangle >= triangle2}");
+        Console.OutputEncoding = Encoding.UTF8; // Для корректного вывода русского текста
+        LabTasks.RunInteractive();
     }
 }
