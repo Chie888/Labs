24dd0587fc445a2f813d87a1e58a04f2191136b8
Chie888 <gizunova2005@mail.ru>
2025-04-30 03:43:57 +0500

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Text;
+using System.Windows;
+
+namespace RightTriangleApp
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OnExecuteClick(object sender, RoutedEventArgs e)
+        {
+            TextBoxOutput.Clear();
+
+            if (!TryParsePositiveDouble(TextBoxA1.Text, out double a1) ||
+                !TryParsePositiveDouble(TextBoxB1.Text, out double b1) ||
+                !TryParsePositiveDouble(TextBoxA2.Text, out double a2) ||
+                !TryParsePositiveDouble(TextBoxB2.Text, out double b2))
+            {
+                MessageBox.Show("Введите корректные положительные числа для всех катетов. Например: 30,68", "Ошибка ввода", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            RightTriangle triangle1 = new RightTriangle(a1, b1);
+            RightTriangle triangle2 = new RightTriangle(a2, b2);
+
+            var sb = new StringBuilder();
+
+            sb.AppendLine("Первый треугольник:");
+            AppendTriangleInfo(sb, triangle1);
+
+            RightTriangle triangle1Inc = ++triangle1;
+            sb.AppendLine($"После ++: {triangle1Inc}");
+
+            RightTriangle triangle1Dec = --triangle1Inc;
+            sb.AppendLine($"После --: {triangle1Dec}");
+
+            double area1 = (double)triangle1;
+            sb.AppendLine($"Площадь через приведение к double: {area1}");
+
+            bool exists1 = triangle1;
+            sb.AppendLine($"Треугольник существует: {exists1}");
+
+            sb.AppendLine();
+
+            sb.AppendLine("Второй треугольник:");
+            AppendTriangleInfo(sb, triangle2);
+
+            RightTriangle triangle2Inc = ++triangle2;
+            sb.AppendLine($"После ++: {triangle2Inc}");
+
+            RightTriangle triangle2Dec = --triangle2Inc;
+            sb.AppendLine($"После --: {triangle2Dec}");
+
+            double area2 = (double)triangle2;
+            sb.AppendLine($"Площадь через приведение к double: {area2}");
+
+            bool exists2 = triangle2;
+            sb.AppendLine($"Треугольник существует: {exists2}");
+
+            sb.AppendLine();
+
+            sb.AppendLine("Сравнение треугольников по площади:");
+            sb.AppendLine($"triangle1 <= triangle2: {triangle1 <= triangle2}");
+            sb.AppendLine($"triangle1 >= triangle2: {triangle1 >= triangle2}");
+
+            TextBoxOutput.Text = sb.ToString();
+        }
+
+        private bool TryParsePositiveDouble(string text, out double value)
+        {
+            return double.TryParse(text, out value) && value > 0;
+        }
+
+        private void AppendTriangleInfo(StringBuilder sb, RightTriangle triangle)
+        {
+            sb.AppendLine(triangle.ToString());
+            sb.AppendLine($"Площадь: {triangle.CalculateArea()}");
+        }
+    }
+}
---
+++ RightTriangle.cs
@@ -0,0 +1,123 @@
+﻿using System;
+
+/// <summary>
+/// Класс, представляющий прямоугольный треугольник с двумя катетами.
+/// </summary>
+public class RightTriangle
+{
+    private double _a;
+    private double _b;
+
+    /// <summary>
+    /// Длина первого катета.
+    /// </summary>
+    public double A
+    {
+        get { return _a; }
+        set { _a = value; }
+    }
+
+    /// <summary>
+    /// Длина второго катета.
+    /// </summary>
+    public double B
+    {
+        get { return _b; }
+        set { _b = value; }
+    }
+
+    /// <summary>
+    /// Конструктор по умолчанию.
+    /// </summary>
+    public RightTriangle()
+    {
+        _a = 0;
+        _b = 0;
+    }
+
+    /// <summary>
+    /// Конструктор с параметрами.
+    /// </summary>
+    /// <param name="a">Длина первого катета.</param>
+    /// <param name="b">Длина второго катета.</param>
+    public RightTriangle(double a, double b)
+    {
+        _a = a;
+        _b = b;
+    }
+
+    /// <summary>
+    /// Вычисляет площадь прямоугольного треугольника.
+    /// </summary>
+    /// <returns>Площадь треугольника.</returns>
+    public double CalculateArea()
+    {
+        return 0.5 * _a * _b;
+    }
+
+    /// <summary>
+    /// Возвращает строковое представление треугольника.
+    /// </summary>
+    /// <returns>Строка с длинами катетов.</returns>
+    public override string ToString()
+    {
+        return $"Катеты: a = {_a}, b = {_b}";
+    }
+
+    /// <summary>
+    /// Увеличивает длины катетов в 2 раза.
+    /// </summary>
+    /// <param name="triangle">Исходный треугольник.</param>
+    /// <returns>Новый треугольник с увеличенными катетами.</returns>
+    public static RightTriangle operator ++(RightTriangle triangle)
+    {
+        return new RightTriangle(triangle._a * 2, triangle._b * 2);
+    }
+
+    /// <summary>
+    /// Уменьшает длины катетов в 2 раза.
+    /// </summary>
+    /// <param name="triangle">Исходный треугольник.</param>
+    /// <returns>Новый треугольник с уменьшенными катетами.</returns>
+    public static RightTriangle operator --(RightTriangle triangle)
+    {
+        return new RightTriangle(triangle._a / 2, triangle._b / 2);
+    }
+
+    /// <summary>
+    /// Явное приведение к double - площадь треугольника, если он существует, иначе отрицательное число.
+    /// </summary>
+    /// <param name="triangle">Треугольник.</param>
+    public static explicit operator double(RightTriangle triangle)
+    {
+        if (triangle._a > 0 && triangle._b > 0)
+            return triangle.CalculateArea();
+
+        return -1;
+    }
+
+    /// <summary>
+    /// Неявное приведение к bool - true, если треугольник существует, иначе false.
+    /// </summary>
+    /// <param name="triangle">Треугольник.</param>
+    public static implicit operator bool(RightTriangle triangle)
+    {
+        return triangle._a > 0 && triangle._b > 0;
+    }
+
+    /// <summary>
+    /// Сравнивает площади треугольников (меньше или равно).
+    /// </summary>
+    public static bool operator <=(RightTriangle left, RightTriangle right)
+    {
+        return left.CalculateArea() <= right.CalculateArea();
+    }
+
+    /// <summary>
+    /// Сравнивает площади треугольников (больше или равно).
+    /// </summary>
+    public static bool operator >=(RightTriangle left, RightTriangle right)
+    {
+        return left.CalculateArea() >= right.CalculateArea();
+    }
+}
