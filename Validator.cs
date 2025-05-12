8637a77344b7a68fcd7f10310150ff93115c1057
Chie888 <gizunova2005@mail.ru>
2025-05-13 03:04:35 +0500

    Update Validator.cs

--- Validator.cs
+++ Validator.cs
@@ -1,25 +1,29 @@
-﻿using System;
-
 namespace CafeMenuApp
 {
     /// <summary>
-    /// Класс для проверки корректности вводимых данных.
+    /// Класс для проверки корректности вводимых значений.
     /// </summary>
     public static class Validator
     {
         /// <summary>
         /// Проверяет, что целое число неотрицательное.
         /// </summary>
+        /// <param name="value">Проверяемое значение.</param>
+        /// <returns>True, если значение неотрицательное; иначе false.</returns>
         public static bool IsNonNegative(int value) => value >= 0;
 
         /// <summary>
         /// Проверяет, что десятичное число неотрицательное.
         /// </summary>
+        /// <param name="value">Проверяемое значение.</param>
+        /// <returns>True, если значение неотрицательное; иначе false.</returns>
         public static bool IsNonNegative(decimal value) => value >= 0;
 
         /// <summary>
         /// Проверяет, что число с плавающей точкой неотрицательное.
         /// </summary>
+        /// <param name="value">Проверяемое значение.</param>
+        /// <returns>True, если значение неотрицательное; иначе false.</returns>
         public static bool IsNonNegative(double value) => value >= 0;
     }
 }
